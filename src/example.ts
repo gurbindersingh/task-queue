47a7004b74a7b1d1408a7be7dfcd2f20f187a6f6
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2020-05-30 13:11:21 +0200

    Update Example

--- src/example.ts
+++ src/example.ts
@@ -1,17 +1,20 @@
 import { AjaxQueue } from './ajax-queue';
 
+// This function will be passed to the queue later on
 function foo(): Promise<void> {
+    // Generate a random number between 0 and 99
     const randomNum = Math.ceil(Math.random() * 100);
-    console.log('    ', randomNum);
 
+    // Return a resolved Promise if the random number was even
     if (randomNum % 2 === 0) {
         return Promise.resolve();
     }
+    // Default return value is a rejected Promises
     return Promise.reject();
 }
 
+// This function will also be passed to the queue
 function bar(text: string, num: number): Promise<void> {
-    console.log('    ', num);
     if (num % 2 === 1) {
         console.log(text);
         return Promise.resolve();
@@ -19,8 +22,14 @@ function bar(text: string, num: number): Promise<void> {
     return Promise.reject();
 }
 
-const queue = new AjaxQueue();
+// Initialise Queue
+const QUEUE = new AjaxQueue();
 
-queue.add(foo);
-queue.add(bar, 'The quick brown fox', Math.ceil(Math.random() * 100));
-queue.add(() => Promise.resolve());
+// Calling a function which takes no parameters
+QUEUE.add(foo);
+
+// The syntax for passing parameters to a function is the same as for the setIntervall() method
+QUEUE.add(bar, 'The quick brown fox', Math.ceil(Math.random() * 100));
+
+// Obviously we can also pass a arrow function
+QUEUE.add(() => Promise.resolve());
